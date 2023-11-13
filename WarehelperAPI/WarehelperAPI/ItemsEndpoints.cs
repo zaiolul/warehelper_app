7b6e6cea99dca51e955ce842238e4ac90f6937d5
zaiolul <89912978+zaiolul@users.noreply.github.com>
2023-11-13 15:06:29 +0200

    not found request text

--- WarehelperAPI/WarehelperAPI/ItemsEndpoints.cs
+++ WarehelperAPI/WarehelperAPI/ItemsEndpoints.cs
@@ -64,7 +64,7 @@ namespace WarehelperAPI
                 Item item = await dbContext.Items.Include(it => it.Warehouse).Include(it => it.Warehouse.Company).FirstOrDefaultAsync<Item>(it => it.Id == itemId && it.Warehouse.Id == warehouseId && it.Warehouse.Company.Id == companyId);
                 if (item == null)
                 {
-                    return Results.NotFound();
+                    return Results.NotFound("Item not found");
                 }
 
                 item.Description = updateItemDto.Description;
