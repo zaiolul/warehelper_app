69557f643541064c5b1b60f0f8c84f098f50806e
zaiolul <89912978+zaiolul@users.noreply.github.com>
2023-12-17 23:35:08 +0200

    change api url

--- WarehelperAPI/reactclient/src/utils/ApiUrl.js
+++ WarehelperAPI/reactclient/src/utils/ApiUrl.js
@@ -9,6 +9,6 @@ const prod = {
     URL: API_REMOTE,
 };
 
-const API = process.env.NODE_ENV === 'development' ? dev : prod;
+const API = process.env.NODE_ENV === 'development' ? prod : dev; //neina pakeist nzn
 
 export default API;
